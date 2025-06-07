5f809a11979868073f36d13ee2daad0a54827514
bibhanshu12 <mail.bibhanshu127@gmail.com>
2025-06-07 23:10:06 +0530

    adds merge_two_Sorted_list function only!

---
+++ merge_Two_Sorted_list.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class merge_Two_Sorted_list {
+    
+ public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+
+            tail = tail.next;
+
+        }
+
+        if (list1 != null)
+            tail.next = list1;
+        if (list2 != null)
+            tail.next = list2;
+
+        return dummy.next;
+
+    }
+ 
+}
