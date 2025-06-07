7c807f212db36303ffa8a6266c0f37b5dac8c23d
bibhanshu12 <mail.bibhanshu127@gmail.com>
2025-06-07 14:20:15 +0530

    adds missing_number code

--- missing_numbers.java
+++ missing_numbers.java
@@ -8,5 +8,24 @@ class missing_numbers {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
+        System.out.println("The missing No. is: "+ missingNo(arr));
     }
+
+    public static int missingNo(int[] arr){
+        if(arr.length<0){
+            return 0;
+        }
+int initialSum=0;
+int n=arr.length;
+        for(int i =0;i<n;i++){
+                initialSum+=arr[i];
+        }
+
+        int sum=(n*(n+1))/2;
+
+        return sum-initialSum;
+
+    }
+    
 }
